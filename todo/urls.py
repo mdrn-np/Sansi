447f8347655db465b897bc7ce0b0e46ce1acfe64
gauravphuyal <phuyalgaurav90@gmail.com>
2024-06-23 08:59:23 +0545

    Project Redo

--- todo/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import TodoViewSet
-
-from rest_framework.routers import SimpleRouter
-
-router = SimpleRouter()
-router.register("", TodoViewSet, basename="todo")
-urlpatterns = router.urls
